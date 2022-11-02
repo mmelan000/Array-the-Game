6e5d4d41bd9c4c68d074e2b246c46e0a78efe4c2
Cloud <m.melanson000@gmail.com>
2022-11-02 15:08:45 -0400

    added filter and map to check each players exisiting claimed tiles

--- client/src/utils/Endgame.js
+++ client/src/utils/Endgame.js
@@ -86,17 +86,17 @@ const testboard = [
   },
   {
     position: 18,
-    player: 'unclaimed',
+    player: 'blue',
     display: '4',
   },
   {
     position: 19,
-    player: 'unclaimed',
+    player: 'blue',
     display: '4',
   },
   {
     position: 20,
-    player: 'unclaimed',
+    player: 'blue',
     display: '8',
   },
   {
@@ -111,7 +111,7 @@ const testboard = [
   },
   {
     position: 23,
-    player: 'unclaimed',
+    player: 'blue',
     display: '9',
   },
   {
@@ -182,38 +182,43 @@ const testboard = [
 ];
 function victoryChecker(playerClaims) {
   // 16 different winning positions
-  // horizontal (6):
-  //                [1,2,3,4,5,6]
-  //                [7,8,9,10,11,12]
-  //                [13,14,15,16,17,18]
-  //                [19,20,21,22,23,24]
-  //                [25,26,27,28,29,30]
-  //                [31,32,33,34,35,36]
-  // vertical (6):                      +6
-  //                [1,7,13,19,25,31]
-  //                [2,8,14,20,26,32]
-  //                [3,9,15,21,27,33]
-  //                [4,10,16,22,28,34]
-  //                [5,11,17,23,29,35]
-  //                [6,12,18,24,30,36]
-  // diagonal (2):
-  //                [1,8,15,22,29,36]   +7
-  //                [6,11,16,21,26,31]  +5
+  const winningPositions = [
+    [1, 2, 3, 4, 5, 6],
+    [7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18],
+    [19, 20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29, 30],
+    [31, 32, 33, 34, 35, 36],
+    [1, 7, 13, 19, 25, 31],
+    [2, 8, 14, 20, 26, 32],
+    [3, 9, 15, 21, 27, 33],
+    [4, 10, 16, 22, 28, 34],
+    [5, 11, 17, 23, 29, 35],
+    [6, 12, 18, 24, 30, 36],
+    [1, 8, 15, 22, 29, 36],
+    [6, 11, 16, 21, 26, 31],
+  ];
+  const toBeChecked = playerClaims;
+
+  console.log('something');
 }
 
 function Endgame(boardState) {
-  const redBoard = boardState.filter(
-    (boardspace) => boardspace.player === 'red'
-  );
-  const yellowBoard = boardState.filter(
-    (boardspace) => boardspace.player === 'yellow'
-  );
-  const blueBoard = boardState.filter(
-    (boardspace) => boardspace.player === 'blue'
-  );
+  const redBoard = boardState
+    .filter((a) => a.player === 'red')
+    .map((b) => b.position);
+  const yellowBoard = boardState
+    .filter((a) => a.player === 'yellow')
+    .map((b) => b.position);
+  const blueBoard = boardState
+    .filter((a) => a.player === 'blue')
+    .map((b) => b.position);
+
   console.log(redBoard);
   console.log(yellowBoard);
   console.log(blueBoard);
+
+  victoryChecker(blueBoard);
 }
 
 Endgame(testboard);
