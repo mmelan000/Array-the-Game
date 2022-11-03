0fda118e474e6f65fc5073c8837cf2f6fdf55815
Zoot83 <marshallrizzuto@gmail.com>
2022-11-03 19:09:47 -0400

    auth middleware added

--- server/server.js
+++ server/server.js
@@ -1,41 +1,58 @@
-import express from 'express';
-import path from 'path';
-import db from './config/connection';
-import { ApolloServer } from 'apollo-server-express';
-import { authMiddleware } from './utils/auth';
+// import express from 'express';
+// import path from 'path';
+// import db from './config/connection';
+// import { ApolloServer } from 'apollo-server-express';
+// import { authMiddleware } from './utils/auth';
 
-import { typeDefs, resolvers } from './schemas';
+// import { typeDefs, resolvers } from './schemas';
 
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(
+//         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+//       );
+//     });
+//   });
+// };
+
+// startApolloServer(typeDefs, resolvers);
+
+const express = require('express');
+const db = require('./config/connection');
+
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
 
-app.use(express.urlencoded({ extended: false }));
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
-};
-
-startApolloServer(typeDefs, resolvers);
+});
