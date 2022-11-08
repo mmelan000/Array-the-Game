8e2c9dce426fe45aa69410c2c62f13f5c3d4f74c
Zoot83 <marshallrizzuto@gmail.com>
2022-11-08 12:32:50 -0500

    single thought query

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -12,15 +12,15 @@ const ThoughtList = ({ thoughts, title }) => {
       {thoughts &&
         thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {thought.thoughtAuthor} <br />
-              <span style={{ fontSize: '1rem' }}>
-                had this thought on {thought.createdAt}
-              </span>
-            </h4>
             <div className="card-body bg-light p-2">
               <p>{thought.thoughtText}</p>
             </div>
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/thoughts/${thought._id}`}
+            >
+              Join the discussion on this thought.
+            </Link>
           </div>
         ))}
     </div>
