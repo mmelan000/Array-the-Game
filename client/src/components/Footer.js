1e69aa8971e35e0ec705b68c809bf172dd176e35
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-11-08 20:21:35 -0500

    Tweaked CSS Updated Footer
    - Tweaked CSS for lobby page
    - Updated Footer
    - Updated App.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function Footer() {
-  console.log('Footer.js');
+export default function Footer(props) {
   return (
     <footer>
-      <p>This is the FOOTER.</p>
+      <p>A Game by Mike Melanson, Alvin Pollard & Marshall Rizzuto.</p>
+      <p>
+        Copyright {'\u00A9'} {props.year}
+      </p>
     </footer>
   );
 }
