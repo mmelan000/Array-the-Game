3003d07e425b997e3f3f83c0f06391614ec9b178
Cloud <m.melanson000@gmail.com>
2022-11-10 19:53:49 -0500

    more socket io stuff

---
+++ client/src/utils/newBoard.js
@@ -0,0 +1,146 @@
+export const newBoard = {
+  1: {
+    player: 'unclaimed',
+    display: '2',
+  },
+  2: {
+    player: 'unclaimed',
+    display: '3',
+  },
+  3: {
+    player: 'unclaimed',
+    display: '4',
+  },
+  4: {
+    player: 'unclaimed',
+    display: '5',
+  },
+  5: {
+    player: 'unclaimed',
+    display: '6',
+  },
+  6: {
+    player: 'unclaimed',
+    display: '2',
+  },
+  7: {
+    player: 'unclaimed',
+    display: '6',
+  },
+  8: {
+    player: 'unclaimed',
+    display: '7',
+  },
+  9: {
+    player: 'unclaimed',
+    display: '8',
+  },
+  10: {
+    player: 'unclaimed',
+    display: '9',
+  },
+  11: {
+    player: 'unclaimed',
+    display: '7',
+  },
+  12: {
+    player: 'unclaimed',
+    display: '3',
+  },
+  13: {
+    player: 'unclaimed',
+    display: '5',
+  },
+  14: {
+    player: 'unclaimed',
+    display: '9',
+  },
+  15: {
+    player: 'unclaimed',
+    display: '12',
+  },
+  16: {
+    player: 'unclaimed',
+    display: '12',
+  },
+  17: {
+    player: 'unclaimed',
+    display: '8',
+  },
+  18: {
+    player: 'unclaimed',
+    display: '4',
+  },
+  19: {
+    player: 'unclaimed',
+    display: '4',
+  },
+  20: {
+    player: 'unclaimed',
+    display: '8',
+  },
+  21: {
+    player: 'unclaimed',
+    display: '12',
+  },
+  22: {
+    player: 'unclaimed',
+    display: '12',
+  },
+  23: {
+    player: 'unclaimed',
+    display: '9',
+  },
+  24: {
+    player: 'unclaimed',
+    display: '5',
+  },
+  25: {
+    player: 'unclaimed',
+    display: '3',
+  },
+  26: {
+    player: 'unclaimed',
+    display: '7',
+  },
+  27: {
+    player: 'unclaimed',
+    display: '9',
+  },
+  28: {
+    player: 'unclaimed',
+    display: '8',
+  },
+  29: {
+    player: 'unclaimed',
+    display: '7',
+  },
+  30: {
+    player: 'unclaimed',
+    display: '6',
+  },
+  31: {
+    player: 'unclaimed',
+    display: '2',
+  },
+  32: {
+    player: 'unclaimed',
+    display: '6',
+  },
+  33: {
+    player: 'unclaimed',
+    display: '5',
+  },
+  34: {
+    player: 'unclaimed',
+    display: '4',
+  },
+  35: {
+    player: 'unclaimed',
+    display: '3',
+  },
+  36: {
+    player: 'unclaimed',
+    display: '2',
+  },
+};
