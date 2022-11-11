c73fef54a62fd1f001cb8219b24997cfc41d2371
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-11-11 15:23:49 -0500

    Team card changes
    - Added Team card changes
    - Added Team container changes
    - As per Mike
    
    - Styled Landing Page and mobile Portrait view

--- client/src/components/TeamCard.js
+++ client/src/components/TeamCard.js
@@ -1,7 +1,39 @@
-export default function TeamCard(props) {
+import redTile from '../images/Board/RedBoardTile.svg';
+import blueTile from '../images/Board/BlueBoardTile.svg';
+import greenTile from '../images/Board/GreenBoardTile.svg';
+
+export default function TeamCard({ player, color, isTurn }) {
+  let emblem;
+  let altText;
+  switch (color) {
+    default:
+      console.log('Check teamcard.js');
+      break;
+
+    case 'Red':
+      emblem = redTile;
+      altText =
+        'Sun with sun rays coming off of it and enclosed by a square red border';
+      break;
+
+    case 'Blue':
+      emblem = blueTile;
+      altText =
+        'Blue circle with water droplets in the top left and bottom right and enclosed by a square blue border';
+      break;
+
+    case 'Green':
+      emblem = greenTile;
+      altText =
+        'Green circle with leaves in the top left and bottom right and enclosed by a square green border';
+      break;
+  }
   return (
-    <div className='team-card'>
-      <p>Team {props.team}</p>
+    <div className={isTurn ? 'is-turn' : ''}>
+      <div className='team-card'>
+        <img src={emblem} alt={altText}></img>
+        <p>{player}</p>
+      </div>
     </div>
   );
 }
