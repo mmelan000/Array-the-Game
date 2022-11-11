50724f5f81a4d049d8db339afd70e8c50ee11787
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-11-11 00:47:41 -0500

    modified:   client/src/App.css 	modified:   client/src/pages/Home.js 	modified:   client/src/pages/Lobby.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,11 @@
 import { React, useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
 import Modal from 'react-bootstrap/Modal';
+import validArrays from '../images/ValidArrays.png';
+import sunWins from '../images/SunWins.png';
+import welcome from '../images/Welcome.png';
+import claim from '../images/Claim.png';
+import rollTen from '../images/RollTen.png';
 
 import Carousel from 'react-bootstrap/Carousel';
 const { v4: uuidv4 } = require('uuid');
@@ -8,31 +13,31 @@ const { v4: uuidv4 } = require('uuid');
 const pages = [
   {
     title: 'Welcome',
-    picture: '',
+    picture: welcome,
     information: 'Welcome to [ Array ] a game for 2 to 3 players',
     key: 1,
   },
   {
     title: 'Goal of Game',
-    picture: '',
+    picture: sunWins,
     information: `The object of the game is to make an ARRAY before your opponents do, an ARRAY is 6 tiles in a line with your color on it. (5 if you're playing 3 players)`,
     key: 2,
   },
   {
     title: 'How to play',
-    picture: '',
+    picture: validArrays,
     information: `You can score an Array, Vertically, Horizontally or Diagonally.`,
     key: 3,
   },
   {
     title: 'How to play',
-    picture: '',
+    picture: claim,
     information: `Each turn you will Roll the Dice and click on a tile to claim it with your color.`,
     key: 4,
   },
   {
     title: 'How to play',
-    picture: '',
+    picture: rollTen,
     information: `If all tiles of a number are occupied by your opponents colors, you can choose one of their tiles and turn into your color`,
     key: 5,
   },
@@ -67,28 +72,28 @@ export default function Home({ currentPage, handlePageChange }) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <div className="about-wrapper">
-      <div className="about-section fw-semibold">
+    <div className='about-wrapper'>
+      <div className='about-section fw-semibold'>
         <ul>
-          <li className="list-group-item pt-4">Welcome to [ ARRAY ]!</li>
-          <li className="list-group-item pt-4">
+          <li className='list-group-item pt-4'>Welcome to [ ARRAY ]!</li>
+          <li className='list-group-item pt-4'>
             A simple and fun game for all ages!
           </li>
-          <li className="list-group-item pt-4">
+          <li className='list-group-item pt-4'>
             Learn how to play below or start your own game!
           </li>
         </ul>
-        <div className="button-container pt-5">
+        <div className='button-container pt-5'>
           <Button
-            type="button"
-            className="btn btn-success shadow fs-3 mx-2"
+            type='button'
+            className='btn btn-success shadow fs-3 mx-2'
             href={`/lobby/${uuidv4()}`}
           >
             PLAY
           </Button>
           <Button
-            type="button"
-            className="btn btn-light shadow fs-3 mx-2"
+            type='button'
+            className='btn btn-light shadow fs-3 mx-2'
             onClick={handleShow}
           >
             LEARN
@@ -100,11 +105,11 @@ export default function Home({ currentPage, handlePageChange }) {
                 <Modal.Title>Learn about [ Array ] </Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <Carousel>
+                <Carousel interval={null}>
                   {pages.map((page) => (
                     <Carousel.Item key={page.key}>
                       <img
-                        className="d-block w-100"
+                        className='d-block w-100'
                         src={page.picture}
                         alt={page.information}
                       />
